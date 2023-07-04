90cbd5c3b0569dd183c0730141c4e81ad058adde
Cotezzo <Cotezzo>
2023-07-05 01:11:22 +0200

    Updated NodeJS version

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");	// MongoDB Database connector
 require("dotenv").config();				// Enviroment variables setup
 
-mongoose.connect(process.env.MONGO_URL,
+const mongoDbAddress = process.env.TEST ? process.env.MONGO_URL_TEST : process.env.MONGO_URL
+console.log(`Trying to connect to ${mongoDbAddress}...`);
+
+mongoose.connect(mongoDbAddress,
     {
         useUnifiedTopology: true,
         useNewUrlParser: true,
